906b86f8443a90aaa705377180f2a8cbebf89317
Samuel W. Flint <swflint@flintfam.org>
2024-09-06 11:55:26 -0500

    Add a main method

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ def factorial(x):
     if x <= 1:
         return 1
     return x * factorial(x - 1)
+
+if __name__ == '__main__':
+    number = input("Input a number: ")
+    print(factorial(number))
